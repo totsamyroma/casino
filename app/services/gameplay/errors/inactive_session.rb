5dda83aa84bb6127a97b6fcf945aac814d07b134
totsamyroma <romantehanov@gmail.com>
2024-05-22 12:02:55 +0300

    Added Gameplay::Errors::InactiveSession spec

--- app/services/gameplay/errors/inactive_session.rb
+++ app/services/gameplay/errors/inactive_session.rb
@@ -1,7 +1,9 @@
 module Gameplay
   module Errors
     class InactiveSession < StandardError
-      def initialize(msg="You can't play on an inactive session. Start game to play.")
+      ERROR_MESSAGE = "You can't play on an inactive session. Start game to play."
+
+      def initialize(msg=ERROR_MESSAGE)
         super
       end
     end
---
+++ spec/services/gameplay/errors/inactive_session_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Gameplay::Errors::InactiveSession do
+  subject(:error) { described_class }
+
+  let(:error_message) { "You can't play on an inactive session. Start game to play." }
+
+  it 'has expected error message' do
+    expect { raise error }.to raise_error do |e|
+      expect(e.message).to eq(error_message)
+    end
+  end
+end
