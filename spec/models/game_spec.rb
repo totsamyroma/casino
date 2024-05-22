b3da7152fe86e3f89be98e5ce2654327b20129be
totsamyroma <romantehanov@gmail.com>
2024-05-22 11:39:48 +0300

    Added Game relations unit spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -30,4 +30,19 @@ RSpec.describe Game, type: :model do
       it { is_expected.to_not be_valid }
     end
   end
+
+  describe 'relations' do
+    subject(:game) { create(:game) }
+
+    let!(:player1) { create(:player) }
+    let!(:player2) { create(:player) }
+
+    let!(:session1) { create(:session, player: player1, game:) }
+    let!(:session2) { create(:session, player: player2, game:) }
+
+    it 'has many users and sessions' do
+      expect(game.sessions).to eq([session1, session2])
+      expect(game.players).to eq([player1, player2])
+    end
+  end
 end
