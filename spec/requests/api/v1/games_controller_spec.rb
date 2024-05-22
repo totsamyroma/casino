2ceb98aa3e7ad45f580ebbe31cffb633dc220149
totsamyroma <romantehanov@gmail.com>
2024-05-22 19:32:25 +0300

    Added Api::V1::GamesController spec

---
+++ spec/requests/api/v1/games_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::GamesControllers", type: :request do
+  describe "GET /api/v1/games" do
+    subject(:request) { get "/api/v1/games" }
+
+    let!(:games) { create_list(:game, 3) }
+
+    it 'returns a list of games' do
+      request
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eq(games.to_json)
+    end
+  end
+end
