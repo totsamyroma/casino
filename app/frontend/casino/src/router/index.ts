cf112be18c2fce99ba8af22766bdc0139f3e09f1
totsamyroma <romantehanov@gmail.com>
2024-05-30 01:18:48 +0300

    Removed initial VueJS app files

--- app/frontend/casino/src/router/index.ts
+++ app/frontend/casino/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 import CasinoView from '../views/CasinoView.vue'
 import GamesView from '../views/GamesView.vue'
@@ -10,19 +9,6 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/casino',
       name: 'casino',
       component: CasinoView
     },
